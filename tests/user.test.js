7cf97a17c718097d88f14b60281e6177bb9649c0
jib5549 <jib5549@gmail.com>
2023-07-01 13:46:44 -0400

    test not working idk why grr

--- tests/user.test.js
+++ tests/user.test.js
@@ -18,5 +18,67 @@ afterAll(async () => {
 });
 
 
-// describe, test, afterAll, beforeAll ALL comes from Jest
+describe('Test the user endpoints', () => {
+  test('It should create a new user and cart', async () => {
+      const response = await request(app)
+      .post('/users')
+      .send({ name: 'Jun', password: 'password', email: 'jib5549@gmail.com' })
+
+  expect(response.statusCode).toBe(200)
+  expect(response.body.user.name).toEqual('Jun')
+  expect(response.body.user.email).toEqual('jib5549@gmail.com')
+  expect(response.body).toHaveProperty('token')
+  expect(response.body.user.loggedIn).toEqual(false)
+  })
+  test('It should allow a user to log in', async () => {
+      const user = new User({ name: 'Jun', password: 'password', email: 'jib5549@gmail.com' })
+      await user.save()
+      const response = await request(app)
+          .post('/users/login')
+          .send({ email: 'jib5549@gmail.com', password: 'password' })
+
+      expect(response.statusCode).toBe(200)
+      expect(response.body.user.name).toEqual('Jun')
+      expect(response.body.user.email).toEqual('jib5549@gmail.com')
+      expect(response.body).toHaveProperty('token')
+      expect(response.body.user.loggedIn).toEqual(true)
+  })
+ test('It should update a user', async () => {
+        const user = new User({ name: 'Jun', email: 'jib5549@gmail.com', password: 'password' })
+        await user.save()
+        const token = await user.generateAuthToken()
+
+        const response = await request(app)
+            .put(`/users/${user._id}`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({ name: 'Jane Doe', email: 'jane.doe@example.com' })
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body.name).toEqual('Jane Doe')
+        expect(response.body.email).toEqual('jane.doe@example.com')
+    })
+    test('It should delete a user', async () => {
+        const user = new User({ name: 'Jun', email: 'jib5549@gmail.com', password: 'password' })
+        await user.save()
+        const token = await user.generateAuthToken()
+        const response = await request(app)
+            .delete(`/users/${user._id}`)
+            .set('Authorization', `Bearer ${token}`)
+
+        expect(response.statusCode).toBe(204)
+        
+    })
+    test('It should log out a user', async () => {
+        const user = new User({ name: 'Jun', email: 'jib5549@gmail.com', password: 'password' });
+         await user.save();
+         const token = await user.generateAuthToken()
+        const logoutResponse = await request(app)
+          .post('/users/logout')
+          .set('Authorization', `Bearer ${token}`);
+          
+        expect(logoutResponse.statusCode).toBe(200);
+        expect(logoutResponse.body.message).toEqual('User successfully logged out!');
+        expect(response.body.loggedIn).toEqual(false)
+    })
+})
 
